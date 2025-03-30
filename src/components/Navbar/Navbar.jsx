9f5df2d81902811b0e54982418170586222c1902
sabonawebsite-com <sabonamarara53@gmail.com>
2025-03-31 01:56:02 +0300

    added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import {assets} from '../../assets/assets'
+const Navbar = () => {
+  return (
+    <div>
+        <img src={assets.logo} alt="" />
+      hello
+    </div>
+  )
+}
+
+export default Navbar
